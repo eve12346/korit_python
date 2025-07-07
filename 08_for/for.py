3c82067da6b4edfc1e9bec732af94634d8b983e5
eve12346 <kms38400097@gmail.com>
2025-07-07 15:21:20 +0900

    6day

--- 08_for/for.py
+++ 08_for/for.py
@@ -3,6 +3,8 @@
 for 변수 in 반복할대상:
     반복할 코드
 """
+from pkgutil import resolve_name
+
 #리스트 순회
 fruits = ["사과","바나나","딸기","포도"]
 
@@ -34,3 +36,54 @@ numbers = {3, 1, 4, 1, 5, 9 ,2}
 sorted_numbers = sorted(numbers)
 print(sorted_numbers)
 print(type(sorted_numbers))
+
+word = "python"
+
+for char in word:
+    print(char)
+
+# for i in range(5):
+#     print(i)
+#
+# for i in range(2 , 10 ,2): #2부터 10미만까지, 2씩 증가
+#     print(i)
+
+# for i in range(1, 10):
+#     print(i)
+#     if i == 5:
+#         print("i가 5입니다. 정지!")
+#         break
+#
+# for num in range(1, 10):
+#     if num == 5:
+#         continue
+#     print(num)
+
+#실습문제
+#1부터 100까지 짝수만 출력하기
+# for num in range(2, 101):
+#     if num % 2 == 1:
+#         continue
+#     print(num)
+
+#리스트의 합 구하기
+numbers = [10, 20, 30, 40, 50]
+total = 0
+
+for num in numbers:
+    total  += num
+
+print("리스트의 합:",total)
+
+for dan in range(1, 10):
+    for n in range(1, 10):
+        print(f"{dan}x{n} = {dan * n}")
+
+#평균구하기ㅏ
+scores = [80, 90, 75, 88, 92]
+total = 0
+
+for score in scores:
+    total += score
+average = total / len(scores)
+print("평균 점수:", average)
