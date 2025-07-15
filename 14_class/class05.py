3c1338a782052b4f87e39eca778706e8bed786ff
eve12346 <kms38400097@gmail.com>
2025-07-15 15:18:32 +0900

    12day

--- 14_class/class05.py
+++ 14_class/class05.py
@@ -5,24 +5,27 @@
 #만약 잔액이 부족하다면 출금을 할 수 없도록 출력
 
 class Account:
-    def __init__(self,owner,balance):
+    def __init__(self,owner,balance=0):
         self.owner = owner
         self.balance = balance
+        print(f"{self.owner}님의 계좌가 개설 되었습니다. 잔액은 {self.balance}원 입니다.")
 
-    def account1(self):
-        print(f"{self.owner}님의 잔액은 {self.balance}원 입니다.")
+    def deposit(self,amount):
+        if amount > 0:
+            self.balance += amount
+            print(f"{amount}원 입금 되었습니다. 입금된 후 {self.owner}님의 잔액은 {self.balance}원 입니다")
+        else:
+            print("입금액은 0보다 커야 합나다.")
 
+    def withdraw(self, amount):
+        if amount <= 0: #출금액이 0보다 작은 상황
+            print("출금액은 0보다 커야 합니다.")
+        elif self.balance >= amount: #정상적인 상황
+            self.balance -= amount
+            print(f"{amount}원만큼 출금 되었습니다. 출금된 후 {self.owner}님의 잔액은{self.balance}원 입니다.")
+        else: #출금액이 0이상이면서 잔액보다 출금액이 더 많은 상황
+            print(f"잔액이 부족합니다. 현재 잔액:{self.balance}원")
 
-    def deposit(self, deposit):
-        self.balance = deposit
-        print(f"입금된후 {self.owner}님의 잔액은 {self.deposit}원 입니다")
-
-    def sithdraw(self, sithdraw):
-        self.balance = sithdraw
-        print(f"출금된후 {self.owner}님의 잔액은{self.sithdraw}원 입니다.")
-
-account1 = Account("곽민성",100000)
-account2 = Account("곽민성",150000)
-account3 = Account("곽민성", 50000)
-
-account1.introduce()
+account1 = Account("곽민성",1000000)
+account1.deposit(200000)
+account1.withdraw(1000000)
